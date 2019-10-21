a4844ffa9c792d2b3e7a5dd6bd272e79ad45db6e
skrulling <mikkeleb@gmail.com>
2019-10-21 10:38:32 +0200

    Fixed install script

--- install.sh
+++ install.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 cp -r chc_sheets ~/Documents/
-if [ -d "~/bin" ] ; then
+echo "Copied sheets to ~/Documents/chc_sheets"
+
+if [ ! -d "~/bin" ] ; then
+        echo "Added a new folder /bin to HOME"
         mkdir ~/bin
 fi
-cp chc ~/bin
+cp chc ~/bin/
 
 if [ -z "$ZSH_NAME" ] ; then
-        echo PATH=$PATH:$HOME/bin >> ~/.zshrc
+        echo "PATH=\"\$PATH:\$HOME/bin\"" >> ~/.zshrc
         echo "Added /bin to PATH"
 elif [ -z "$BASH_VERSION" ] ; then
-        echo PATH=$PATH:$HOME/bin >> ~/.bashrc
+        echo "PATH=\"\$PATH:\$HOME/bin\"" >> ~/.bashrc
         echo "Added /bin to PATH"
 else
         echo "Did not find supported shell, try Bash or Zsh"
