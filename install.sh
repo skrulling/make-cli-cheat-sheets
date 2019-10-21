57d0db9325f4feec778a49cabbc4685b312596dd
skrulling <mikkeleb@gmail.com>
2019-10-21 10:54:50 +0200

    More bugfixes to install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash 
 
 cp -r chc_sheets ~/Documents/
 echo "Copied sheets to ~/Documents/chc_sheets"
@@ -12,9 +12,11 @@ cp chc ~/bin/
 if [ -z "$ZSH_NAME" ] ; then
         echo "PATH=\"\$PATH:\$HOME/bin\"" >> ~/.zshrc
         echo "Added /bin to PATH"
+        echo "Remember to run \"source ~/.zshrc\" for changes to take effect"
 elif [ -z "$BASH_VERSION" ] ; then
         echo "PATH=\"\$PATH:\$HOME/bin\"" >> ~/.bashrc
         echo "Added /bin to PATH"
+        echo "Remember to run \"source ~/.bashrc\" for changes to take effect"
 else
         echo "Did not find supported shell, try Bash or Zsh"
 fi
